d65e13f8f7061fe7447ebf2cd862fcb155e06391
Hasib Muharam <hasibmuha@gmail.com>
2025-06-09 06:41:02 +0700

    feat: install NativeWind and configure tailwind

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,13 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
+ 
+const config = mergeConfig(getDefaultConfig(__dirname), {
+  /* your config */
+});
+ 
+const reanimatedConfig = wrapWithReanimatedMetroConfig(config);
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withNativeWind(reanimatedConfig, {input: './global.css'});
